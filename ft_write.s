bbeb48ec370c38d228987bd0791cd0bef8ebb559
Dilan Bhujoo <dbhujoo@f2r7s2.clusters.42paris.fr>
2026-05-21 11:43:22 +0200

    update main + ft_write ok

--- ft_write.s
+++ ft_write.s
@@ -1,11 +1,21 @@
 global  ft_write
+extern __errno_location
+
+section .text
 
 ft_write:
-    mov rax 1   ;
+    mov rax, 1
 syscall
     cmp rax, 0
-    jge exit
+    jge .exit
+
+    push    rax
+    call    __errno_location wrt ..plt
+    pop rcx
+    neg rcx
+    mov [rax], ecx
+    mov rax, -1
     ret
 
-exit:
+.exit:
     ret
