75a0314c44eef95291087dba8fd76138410bca50
Dilan Bhujoo <dbhujoo@f2r7s2.clusters.42paris.fr>
2026-05-22 10:27:14 +0200

    strdup a test, errno a verifier

--- ft_strdup.s
+++ ft_strdup.s
@@ -4,12 +4,24 @@ extern __errno_location
 extern malloc
 
 ft_strdup:
-    push rdi
-    call ft_strlen
-    inc rax
-    call malloc
-    cmp rax, 0
-    je
+    push    rdi
+    call    ft_strlen
+    inc     rax
+    mov     rdi, rax
+    call    malloc
+    cmp     rax, 0
+    je      .exit
+    pop     rsi
+    mov     rdi, rax
+    push    rax
+    call    ft_strcpy
+    pop     rax
+    ret
 
 .exit:
-
+    pop     rdi
+    push    rax
+    call    __errno_location wrt ..plt
+    pop     rax
+    mov     rax, 0
+    ret
